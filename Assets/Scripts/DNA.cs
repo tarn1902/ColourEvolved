17c1a450cc5893e2daab2543dbaa9effe2863244
tarn1902 <42328932+tarn1902@users.noreply.github.com>
2020-11-22 12:51:16 +1100

    Copy of ColourEvoled Project

---
+++ Assets/Scripts/DNA.cs
@@ -0,0 +1,94 @@
+﻿/*----------------------------------------
+File Name: DNA.cs
+Purpose: Controls this objects material
+with genetic algorthim
+Author: Tarn Cooper
+Modified: 07 June 2020
+------------------------------------------
+Copyright 2020 Tarn Cooper.
+-----------------------------------*/
+using UnityEngine;
+//Class for DNA
+public class DNA : MonoBehaviour
+{
+    public Texture texture;
+    public Shader shader;
+    public Color colour;
+    public Color targetColour;
+    public float mutationRate = 50;
+    public float changingSize = 0.5f;
+    RaycastHit hit;
+    RenderTexture rt;
+    Renderer rend;
+
+    //-----------------------------------------------------------
+    // Gets renderer of object this script is attached to
+    //-----------------------------------------------------------
+    private void Awake()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    //-----------------------------------------------------------
+    // Creates new material based on given public objects
+    //-----------------------------------------------------------
+    public void SetNewMaterial()
+    {
+        rend.material = new Material(shader);
+        rend.material.mainTexture = texture;
+        rend.material.color = colour;
+    }
+
+    //-----------------------------------------------------------
+    // Changes colour of current material
+    //-----------------------------------------------------------
+    public void SetExistingMaterial()
+    {
+        rend.material.color = colour;
+    }
+
+    //-----------------------------------------------------------
+    // Calculates fitness level
+    //-----------------------------------------------------------
+    public float CheckFitness()
+    {
+        return Mathf.Abs(targetColour.r - colour.r) + Mathf.Abs(targetColour.g - colour.g) + Mathf.Abs(targetColour.b - colour.b);
+    }
+
+    //-----------------------------------------------------------
+    // Combines script data into a new one
+    // partner1 (DNA): What is the first parent
+    // partner2 (DNA): What is the second parent
+    //-----------------------------------------------------------
+    public void Breed(DNA partner1, DNA partner2)
+    {
+        //Crossbreed
+        colour.r = Random.Range(0, 2) == 0 ? partner1.colour.r : partner2.colour.r;
+        colour.g = Random.Range(0, 2) == 0 ? partner1.colour.g : partner2.colour.g;
+        colour.b = Random.Range(0, 2) == 0 ? partner1.colour.b : partner2.colour.b;
+
+        //Mutation
+        if (mutationRate / 100 > Random.Range(0.0f, 1.0f))
+        {
+            switch (Random.Range(0, 4))
+            {
+                case 0:
+                    {
+                        colour.r += Random.Range(0, 2) == 0 ? changingSize : -changingSize;
+                        break;
+                    }
+                case 1:
+                    {
+                        colour.g += Random.Range(0, 2) == 0 ? changingSize : -changingSize;
+                        break;
+                    }
+                case 3:
+                    {
+                        colour.b += Random.Range(0, 2) == 0 ? changingSize : -changingSize;
+                        break;
+                    }
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/GeneticAlgorthm.cs
@@ -0,0 +1,142 @@
+﻿/*----------------------------------------
+File Name: GeneticAlgorthm.cs
+Purpose: Creates and controls genetics of
+DNA scripted objects
+Author: Tarn Cooper
+Modified: 07 June 2020
+------------------------------------------
+Copyright 2020 Tarn Cooper.
+-----------------------------------*/
+using System.Collections.Generic;
+using UnityEngine;
+
+//Class for genetic algorthm
+public class GeneticAlgorthm : MonoBehaviour
+{
+    public int populationSize = 10;
+    public GameObject prefab;
+    List<GameObject> population;
+    public float evolveWaitTime = 10;
+    float evolveWaitingTime = 0;
+    public int pixelAccuracy = 10;
+    float lowestFitness = 3;
+    float minBreedSize = 1.0f;
+    List<GameObject> roulette;
+    Texture2D texture;
+
+    //-----------------------------------------------------------
+    // Gets all components needed and creates population
+    //-----------------------------------------------------------
+    void Start()
+    {
+        population = new List<GameObject>();
+        roulette = new List<GameObject>();
+        texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        GameObject popObject;
+        for (int i = 0; i < populationSize; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-4.0f, 4.0f), 0, Random.Range(-4.0f, 4.0f));
+            popObject = Instantiate(prefab, pos, Quaternion.identity);
+            popObject.GetComponent<DNA>().colour = new Color(1, 1, 1);
+            popObject.GetComponent<DNA>().SetNewMaterial();
+            population.Add(popObject);
+        }
+    }
+
+    //-----------------------------------------------------------
+    // Breeds population after certain amount of time
+    //-----------------------------------------------------------
+    void Update()
+    {
+        if (evolveWaitingTime >= evolveWaitTime)
+        {
+            BreedPopulation();
+            evolveWaitingTime = 0;
+        }
+        evolveWaitingTime += Time.deltaTime;
+    }
+
+    //-----------------------------------------------------------
+    // Breeds population together
+    //-----------------------------------------------------------
+    void BreedPopulation()
+    {
+        //Creates roulette
+        float totalFitness = MakeRoulette();
+
+        //Breeds two pop with roulette
+        Breed();
+
+        //Recalculates culling point
+        if (totalFitness / populationSize < lowestFitness)
+        {
+            lowestFitness = totalFitness / populationSize;
+            minBreedSize = (populationSize / totalFitness)/3;
+        }
+
+        roulette.Clear();
+    }
+    //-----------------------------------------------------------
+    // Calculates average colour of screen to be used as target 
+    // colour for pops
+    //-----------------------------------------------------------
+    private void OnPostRender()
+    {
+        texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0, false);
+        Color[] pixels = texture.GetPixels();
+        Color totalPixels = Color.white;
+        for (int i = 0; i < pixels.Length; i += pixelAccuracy)
+        {
+            totalPixels += pixels[i];
+        }
+
+        foreach (GameObject pop in population)
+        {
+            DNA dna = pop.GetComponent<DNA>();
+            dna.targetColour = totalPixels / (pixels.Length / pixelAccuracy);
+            dna.SetExistingMaterial();
+        }
+    }
+
+    //-----------------------------------------------------------
+    // Creates roultette with advantage towards most fit
+    //-----------------------------------------------------------
+    private float MakeRoulette()
+    {
+        //Create roulette
+        float totalFitness = 0;
+        foreach (GameObject pop in population)
+        {
+            float fitness = pop.GetComponent<DNA>().CheckFitness();
+            totalFitness += fitness;
+            while (fitness <= lowestFitness)
+            {
+                fitness += minBreedSize;
+                roulette.Add(pop);
+            }
+            
+        }
+        return totalFitness;
+    }
+
+    //-----------------------------------------------------------
+    // Breed to pop based on roulette
+    //-----------------------------------------------------------
+    private void Breed()
+    {
+        //Breed population
+        if (roulette.Count != 0)
+        {
+            for (int i = 0; i < populationSize; i++)
+            {
+                population[i].GetComponent<DNA>().Breed(roulette[Random.Range(0, roulette.Count)].GetComponent<DNA>(), roulette[Random.Range(0, roulette.Count)].GetComponent<DNA>());
+            }
+        }
+        else
+        {
+            lowestFitness += 0.1f;
+        }
+    }
+}
+
+
